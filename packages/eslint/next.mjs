e935ab296c4a86547eca70a61dd945094a9d2a7d
Kohi <kohilaice@gmail.com>
2025-12-10 16:13:39 -0500

    back to FlatCompat

--- packages/eslint/next.mjs
+++ packages/eslint/next.mjs
@@ -1,31 +1,17 @@
-// import { FlatCompat } from '@eslint/eslintrc'
-import pluginNext from '@next/eslint-plugin-next'
+import { FlatCompat } from '@eslint/eslintrc'
 import { defineConfig } from 'eslint/config'
 
 import reactEslintConfig from '@homestuck/eslint-config/react'
 
-/* const compat = new FlatCompat({
+const compat = new FlatCompat({
   baseDirectory: import.meta.dirname,
-}) */
+})
 
 const $config = defineConfig(
   reactEslintConfig,
-  /* ...compat.config({
+  ...compat.config({
     extends: ['plugin:@next/next/recommended'],
-  }), */
-  {
-    plugins: {
-      '@next/next': pluginNext,
-    },
-    rules: {
-      ...pluginNext.configs.recommended.rules,
-    },
-    settings: {
-      next: {
-        rootDir: import.meta.dirname,
-      },
-    },
-  },
+  }),
 )
 
 export default $config
